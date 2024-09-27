fe9d4ad75430085a47ad8b2151eaf19a512ad15e
David Alsh <alshdavid@gmail.com>
2024-09-27 10:11:03 +1000

    watch script

--- watch.bash
+++ watch.bash
@@ -1,9 +1,20 @@
-cargo watch \
-  -w packages \
-  -w crates \
-  -w /Volumes/Storage/Development/alshdavid-templates/atlaspack-starter/build.mjs \
-  -- bash -c "\
-    clear;\
-    yarn build-native && \
-    node /Volumes/Storage/Development/alshdavid-templates/atlaspack-starter/build.mjs \
-  "
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+NPM_SCRIPT="build"
+
+if ! [ "$1" = "" ]; then
+  NPM_SCRIPT="$1"
+fi
+
+npx nodemon \
+  --watch "$ATLASPACK_SRC_PATH/packages/**/*" \
+  --watch "$ATLASPACK_SRC_PATH/crates/**/*" \
+  --watch "$SCRIPT_DIR/plugins/**/*" \
+  --watch "$SCRIPT_DIR/src/**/*" \
+  --watch "$SCRIPT_DIR/build.mjs" \
+  --ext "*" \
+  --delay "250ms" \
+  --signal "SIGTERM" \
+  --exec "npm run $NPM_SCRIPT"
